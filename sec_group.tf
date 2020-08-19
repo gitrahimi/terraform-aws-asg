f96659feb2d3e8301efdc358252c006cf0213853
gitrahimi <mrahimi124@gmail.com>
2020-08-19 15:56:47 +0000

    added HTTPS to Sec_Grp

--- sec_group.tf
+++ sec_group.tf
@@ -10,6 +10,22 @@ resource "aws_security_group" "asg-sec-group" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "Allow HTTP Traffic"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTPS Traffic"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
